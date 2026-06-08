4c441b9d5b838631e69720ae583a9a1866f71eb5
hyuckjin.kwon <hyuckjin.kwon@parksystems.com>
2026-06-08 11:39:32 +0900

    feat: add player API endpoint

---
+++ GamePlatform/GamePlatform.Api/Controllers/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using GamePlatform.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamePlatform.Api.Controllers
+{
+    [ApiController]
+    [Route("api/players")]
+    public class PlayerController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Player> GetPlayers()
+        {
+            return
+            [
+                new Player
+            {
+                Id = 1,
+                Name = "HyuckJin",
+                Level = 100,
+                CreatedAt = DateTime.UtcNow
+            }
+            ];
+        }
+    }
+}
