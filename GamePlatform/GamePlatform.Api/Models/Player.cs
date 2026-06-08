351e24bbb4f610f015fbe78ed33340ae7862e5bb
hyuckjin.kwon <hyuckjin.kwon@parksystems.com>
2026-06-08 11:34:12 +0900

    feat: add player model

---
+++ GamePlatform/GamePlatform.Api/Models/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace GamePlatform.Api.Models
+{
+
+    public class Player
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int Level { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
