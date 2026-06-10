ae0dbbc9c0a1696d2c6ae0a1c1ef0dbc38af50a0
hyuckjin.kwon <hyuckjin.kwon@parksystems.com>
2026-06-10 17:22:15 +0900

    feat: add external API provider for Overwatch profile integration

---
+++ GamePlatform/GamePlatform.Api/Application/Features/Overwatch/Interfaces/IOverwatchProfileProvider.cs
@@ -0,0 +1,9 @@
+﻿using GamePlatform.Api.Application.Features.Overwatch.DTOs;
+
+namespace GamePlatform.Api.Application.Features.Overwatch.Interfaces
+{
+    public interface IOverwatchProfileProvider
+    {
+        Task<OverwatchProfileResponse> FetchAsync(string battleTag);
+    }
+}
